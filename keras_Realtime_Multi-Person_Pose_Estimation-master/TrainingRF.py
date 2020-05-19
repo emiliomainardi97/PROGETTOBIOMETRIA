b383b8091a6918385b37d86657bca11c4d47598b
emiliomainardi97 <e.mainardi@studenti.unisa.it>
2020-05-19 14:21:48 +0200

    Aggiunta Confusion Matrif RF
    Confusion Matrix

--- keras_Realtime_Multi-Person_Pose_Estimation-master/TrainingRF.py
+++ keras_Realtime_Multi-Person_Pose_Estimation-master/TrainingRF.py
@@ -8,6 +8,10 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 import sklearn.metrics as metrics
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import plot_confusion_matrix
+import matplotlib.pyplot as plt
+
+
 import pickle
 
 
@@ -30,9 +34,9 @@ classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
 print(confusion_matrix(y_test, y_pred))
+plt.show()
 print(classification_report(y_test,y_pred))
 print(accuracy_score(y_test, y_pred))
-
 
 kfold = KFold(n_splits=25, shuffle=True, random_state=0)
 
@@ -51,4 +55,18 @@ filename = 'finalized_modelRF.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 predictions = loaded_model.predict(X_test)
 
-print(predictions) 
+print(predictions)
+
+
+titles_options = [("Confusion matrix, without normalization", None),
+                  ("Normalized confusion matrix", 'true')]
+for title, normalize in titles_options:
+    disp = plot_confusion_matrix(classifier, X_test, y_test,
+                                 cmap=plt.cm.Blues,
+                                 normalize=normalize)
+    disp.ax_.set_title(title)
+
+    print(title)
+    print(disp.confusion_matrix)
+    plt.show()
+
